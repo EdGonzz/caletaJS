e33048cee22ce0fa9f44a2d1f4d20ed81c6ed09a
Edwin Contreras <ed25gonz@gmail.com>
2026-01-27 23:59:52 -0400

    refactor: Decouple header rendering from the Home page, adding a dedicated slot in `index.html`, and update its background color.

--- src/pages/Home.js
+++ src/pages/Home.js
@@ -1,11 +1,8 @@
-import Header from "../components/Header";
 import Balance from "../components/Balance";
 import Activos from "../components/Activos";
 
 const Home = () => {
   const view = `
-  ${Header()}
-
   <main class="max-w-md mx-auto pt-20 px-4 pb-24">
     ${Balance()}
     ${Activos()}
