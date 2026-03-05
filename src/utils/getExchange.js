f967b74841e9cf9c35375bcd98e4a2befa441f9d
Edwin Contreras <ed25gonz@gmail.com>
2026-03-04 22:39:55 -0400

    feat: add utility function to fetch exchange data from API

---
+++ src/utils/getExchange.js
@@ -0,0 +1,28 @@
+const API_KEY = process.env.API_KEY;
+const API_URL = process.env.API_URL;
+
+const getExchange = async (id) => {
+  const url = id ? `${API_URL}/exchanges/${id}` : `${API_URL}/exchanges/`
+
+  const options = {
+    method: 'GET',
+    headers: {
+      'x-cg-demo-api-key': API_KEY,
+      'Content-Type': 'application/json'
+    }
+  }
+
+  try {
+    const response = await fetch(url, options)
+    if (!response.ok) {
+      throw new Error(`API Error: ${response.status} ${response.statusText}`)
+    }
+    const data = await response.json()
+    return data
+  } catch (error) {
+    console.error('Error fetching data:', error)
+    return null;
+  }
+}
+
+export default getExchange
